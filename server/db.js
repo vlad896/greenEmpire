34c3c783cb0487a18a4f9cdb9bde21ea7befbbb7
Vladislav Fedorovich <vlados6675041@gmail.com>
2024-04-02 16:42:41 +0300

    delete

--- server/db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-	user: 'postgres',
-	password: 'admin',
-	host: 'localhost',
-	port: 5432,
-	database: 'antresol_org'
-})
-module.exports = pool
